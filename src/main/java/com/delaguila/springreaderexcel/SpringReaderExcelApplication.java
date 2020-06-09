39772f4560a6fc32daaf70871ef9551d9de3c05a
Jhordy Alexi <jhordydelaguila@hotmail.com>
2020-06-09 00:22:41 -0500

    Init

---
+++ src/main/java/com/delaguila/springreaderexcel/SpringReaderExcelApplication.java
@@ -0,0 +1,13 @@
+package com.delaguila.springreaderexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReaderExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReaderExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delaguila/springreaderexcel/SpringReaderExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delaguila.springreaderexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReaderExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
